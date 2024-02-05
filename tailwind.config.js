911636de55ab9ba0b8a7d4941379f6919901a01a
pritom00 <121727623+pritom00@users.noreply.github.com>
2024-02-05 20:19:16 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+
+// remember to use module.exports instead of tailwind.config in production
+tailwind.config = 
+   {
+      // Note: config only includes the used styles & variables of your selection
+      content: ["./src/**/*.{html,vue,svelte,js,jsx,tsx}"],
+      theme: {
+        extend: {
+          fontFamily: {
+            
+          },
+          fontSize: {
+            
+          },
+          fontWeight: {
+            
+          },
+          lineHeight: {
+             
+          },
+          borderRadius: {
+              
+          },
+          colors: {
+            'dark-02': '#424247',
+            
+          },
+          spacing: {
+              
+          },
+          width: {
+             
+          },
+          minWidth: {
+             
+          },
+          maxWidth: {
+             
+          },
+          height: {
+             
+          },
+          minHeight: {
+             
+          },
+          maxHeight: {
+             
+          }
+        }
+      }
+    }
+
+          
